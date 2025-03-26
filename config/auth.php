c5922c30e3fe1f0282f322a0079ca9146ecd2f28
Aryan Malla <aryanmalla19@gmail.com>
2025-03-26 17:08:27 +0545

    JWT setup

--- config/auth.php
+++ config/auth.php
@@ -18,6 +18,12 @@ return [
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
+    'guards' => [
+        'api' => [
+            'driver' => 'jwt',
+            'provider' => 'users',
+        ],
+
     /*
     |--------------------------------------------------------------------------
     | Authentication Guards
