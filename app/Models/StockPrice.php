14466ec0fd5848be3d65432215d32d2b6ee9dbe9
subresh07 <subreshthakulla007@gmail.com>
2025-03-27 14:41:48 +0545

    Relationship is added to each models

--- app/Models/StockPrice.php
+++ app/Models/StockPrice.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class StockPrice extends Model
 {
-    //
+    protected $fillable = ['stock_id', 'price', 'timestamp'];
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'testuser123'
-        ]);
+        User::factory(100)->create();
     }
 }
