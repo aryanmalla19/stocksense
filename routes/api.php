bbe7280d5421eadbffdcc06d6f805274124ae44c
Sandip Magar <reone054@gmail.com>
2025-03-27 13:08:27 +0545

    ready to work

--- routes/api.php
+++ routes/api.php
@@ -5,19 +5,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
-
 # User authentication route api
 Route::post('login', [UserController::class, 'login']);
-Route::get('/users', [UserController::class, 'getUsers']);
 
 Route::group([
     'middleware' => 'api',
     'prefix' => 'user'
-], function ($router) {
+], function () {
     Route::post('logout', [UserController::class, 'logout']);
     Route::post('refresh', [UserController::class, 'refresh']);
     Route::post('me', [UserController::class, 'me']);
