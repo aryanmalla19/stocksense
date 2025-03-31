0d2df55034dcb0c5d18face46bc703edb3980db4
Sandip Magar <reone054@gmail.com>
2025-03-31 16:33:44 +0545

    enable 2fa disable 2fa function

---
+++ app/Http/Controllers/TwoFactorController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+class TwoFactorController extends Controller
+{
+    public function enable(Request $request)
+    {
+        $request->validate([
+            'password' => 'required'
+        ]);
+
+        $token = $request->bearerToken();
+
+        if (!$token) {
+            return response()->json(['error' => 'Token not provided'], 400);
+        }
+
+        $user = JWTAuth::parseToken()->authenticate();
+        
+        if ($user->two_factor_enables) {
+            return "Already enabled";
+        }
+
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        if (!Hash::check($request->password, $user->password)) {
+            return response()->json(['error' => 'Invalid password'], 400);
+        }
+
+        $user->forceFill([
+            'two_factor_enables' => true
+        ])->save();
+
+        return response()->json(['message' => '2FA enabled successfully'], 200);
+    }
+    public function  disable(Request $request){
+        $request->validate([
+            'password' => 'required'
+        ]);
+
+        $token = $request->bearerToken();
+
+        if (!$token) {
+            return response()->json(['error' => 'Token not provided'], 400);
+        }
+
+        $user = JWTAuth::parseToken()->authenticate();
+
+        if (!$user->two_factor_enables) {
+            return "Already disabled";
+        }
+
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        if (!Hash::check($request->password, $user->password)) {
+            return response()->json(['error' => 'Invalid password'], 400);
+        }
+
+        $user->forceFill([
+            'two_factor_enables' => false
+        ])->save();
+
+        return response()->json(['message' => '2FA disabled successfully'], 200);
+    }
+}
