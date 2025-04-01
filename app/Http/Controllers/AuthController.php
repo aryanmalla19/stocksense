0efc92b4fff3603148c06c782331f8cbe54f10dc
subresh07 <subreshthakulla007@gmail.com>
2025-04-01 15:40:47 +0545

    CustomClasses added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,45 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRegisterRequest;
+use App\Http\Requests\UserLoginRequest;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AuthController extends Controller
 {
-    public function register(Request $request): JsonResponse
+    public function register(UserRegisterRequest $request): JsonResponse
     {
-        $request->validate(
-            [
-                'name' => 'required|string|max:50|min:5',
-                'email' => 'required|email|unique:users',
-                'password' => [
-                    'required',
-                    'min:8',
-                    'max:50',
-                    'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/',
-                    'confirmed',
-                ],
-                'password_confirmation' => 'required_with:password',
-            ],
-            [
-                'name.required' => 'Name is missing',
-                'name.max' => 'Name must not exceed 50 characters',
-                'name.min' => 'Name must be at least 5 characters',
-                'email.required' => 'Email is missing',
-                'email.email' => 'Invalid email format',
-                'email.unique' => 'Email is already taken',
-                'password.required' => 'Password is missing',
-                'password.min' => 'Password must be at least 8 characters',
-                'password.max' => 'Password must not exceed 50 characters',
-                'password.regex' => 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
-                'password.confirmed' => 'Password confirmation does not match',
-                'password_confirmation.required_with' => 'Password confirmation is required',
-            ]
-        );
-
         $user = new User;
         $user->name = $request->name;
         $user->email = $request->email;
@@ -61,24 +33,10 @@ class AuthController extends Controller
                 'email' => $user->email,
             ],
         ], 201);
-
     }
 
-    public function login(Request $request)
+    public function login(UserLoginRequest $request): JsonResponse
     {
-        $request->validate(
-            [
-                'email' => 'required | email',
-                'password' => 'required',
-            ],
-            [
-                'email.required' => 'Email missing',
-                'email.email' => 'Not in email format',
-
-                'password.required' => 'Password missing',
-            ]
-        );
-
         $credentials = $request->only(['email', 'password']);
 
         $user = User::where('email', $credentials['email'])->first();
@@ -108,20 +66,16 @@ class AuthController extends Controller
 
     /**
      * Get the authenticated User.
-     *
-     * @return \Illuminate\Http\JsonResponse
      */
-    public function me()
+    public function me(): JsonResponse
     {
         return response()->json(auth('api')->user());
     }
 
     /**
      * Log the user out (Invalidate the token).
-     *
-     * @return \Illuminate\Http\JsonResponse
      */
-    public function logout()
+    public function logout(): JsonResponse
     {
         auth('api')->logout();
 
@@ -130,21 +84,16 @@ class AuthController extends Controller
 
     /**
      * Refresh a token.
-     *
-     * @return \Illuminate\Http\JsonResponse
      */
-    public function refresh()
+    public function refresh(): JsonResponse
     {
         return $this->respondWithToken(auth('api')->refresh());
     }
 
     /**
      * Get the token array structure.
-     *
-     * @param  string  $token
-     * @return \Illuminate\Http\JsonResponse
      */
-    protected function respondWithToken($token)
+    protected function respondWithToken($token): JsonResponse
     {
         return response()->json([
             'access_token' => $token,
--- app/Http/Requests/UserRegisterRequest.php
+++ app/Http/Requests/UserRegisterRequest.php
@@ -11,18 +11,42 @@ class UserRegisterRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
+   
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:50|min:5',
+            'email' => 'required|email|unique:users',
+            'password' => [
+                'required',
+                'min:8',
+                'max:50',
+                'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/',
+                'confirmed',
+            ],
+            'password_confirmation' => 'required_with:password',
+        ];
+    }
+
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Name is missing',
+            'name.max' => 'Name must not exceed 50 characters',
+            'name.min' => 'Name must be at least 5 characters',
+            'email.required' => 'Email is missing',
+            'email.email' => 'Invalid email format',
+            'email.unique' => 'Email is already taken',
+            'password.required' => 'Password is missing',
+            'password.min' => 'Password must be at least 8 characters',
+            'password.max' => 'Password must not exceed 50 characters',
+            'password.regex' => 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+            'password.confirmed' => 'Password confirmation does not match',
+            'password_confirmation.required_with' => 'Password confirmation is required',
         ];
     }
 }
