e24aff71ebbbcdd2d0fa7635bb91a111420b6d84
kewal shrestha <kwlshrestha@gmail.com>
2025-04-02 23:08:28 +0545

    added custom reset notification

---
+++ app/Notifications/CustomResetPassword.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Auth\Notifications\ResetPassword as BaseResetPassword;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class CustomResetPassword extends BaseResetPassword
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($token,$user)
+    {
+        $this->token = $token;
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via( $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail( $notifiable): MailMessage
+    {
+
+        $resetUrl = url(route('password.reset', [
+            'token' => $this->token,
+            'email' => $notifiable->email,
+        ], false));
+
+        return (new MailMessage)
+            ->subject('Reset Your Password')
+            ->view('ResetPassword.custom-reset-password', [
+                'url' => $resetUrl,
+                'user' => $notifiable
+            ]);
+    
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
