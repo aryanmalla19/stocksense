1304532333678802a460a19a835d095982911a0c
kewal shrestha <kwlshrestha@gmail.com>
2025-04-03 08:33:25 +0545

    modified custom reset password response

--- resources/views/ResetPassword/custom-reset-password.blade.php
+++ resources/views/ResetPassword/custom-reset-password.blade.php
@@ -8,10 +8,6 @@
         text-align: center;
     }
 
-    .container {
-        padding: 20px;
-    }
-
     .button {
         background-color: #3498db;
         color: white;
@@ -26,14 +22,33 @@
         max-width: 200px;
         margin-bottom: 20px;
     }
+
+    .container {
+        width: 40%;
+        border: 20px;
+        background-color: white;
+        border-radius: 10px;
+    }
+
+    body {
+        background-color: #eeeeee;
+        display: flex;
+        /* align-items: center; */
+        justify-content: center;
+    }
+
+    .company_name {
+        size: 50px;
+        font-weight: 500;
+    }
     </style>
 </head>
 
 <body>
     <div class="container">
         <img src="{{ asset('images/logo.png') }}" alt="Stock-Sense Logo">
-        <h2>Welcome, {{ $user->name }}!</h2>
-        <p>Click the button below to RESEt your password:</p>
+        <h2>Hello, {{ $user->name }}!</h2>
+        <p>Click the button below to reset your password:</p>
         <p>Please take a moment to ensure we have the right email address for you!</p>
         <a href="{{ $url }}" class="button">Reset Your Password</a>
         <p>If you did not request this, ignore this email.</p>
