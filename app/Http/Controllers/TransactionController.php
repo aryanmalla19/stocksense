87db2b059741365c6b8bce988dd869f1f7686eea
Sandip Magar <reone054@gmail.com>
2025-04-03 19:57:43 +0545

    update transaction method added

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -161,6 +161,9 @@ class TransactionController extends Controller
         ]);
 
         $transaction = Transaction::create($attributes);
+
+        $transaction->load('stock');
+
         return response()->json([
             'message' => 'Successfully created new transaction',
             'data' => new TransactionResource($transaction),
@@ -225,7 +228,7 @@ class TransactionController extends Controller
                 'message' => 'No transaction found with ID ' . $id,
             ], 404);
         }
-        
+
         return response()->json([
             'message' => 'Successfully fetched transaction data',
             'data' => new TransactionResource($transaction),
@@ -324,7 +327,35 @@ class TransactionController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $transaction = Transaction::find($id);
+
+        if (!$transaction) {
+            return response()->json([
+                'message' => 'No Stock found with ID ' . $id,
+            ], 404);
+        }
+
+        $data = $request->validate([
+            'symbol' => 'sometimes|string|max:6|unique:stocks,symbol,' . $id,
+            'name' => 'sometimes|string',
+            'sector_id' => 'sometimes|integer|exists:sectors,id',
+        ]);
+
+        $transaction->forceFill([
+            'user_id' => $request->user_id,
+            'stock_id' => $request->stock_id,
+            'type' => $request->type,
+            'quantity' => $request->quantity,
+            'price' => $request->price,
+            'transaction_fee' => $request->transaction_fee
+        ]);
+        $transaction->save();
+        $transaction->load('stock');
+
+        return response()->json([
+            'message' => 'Stock successfully updated',
+            'data' => new TransactionResource($transaction),
+        ]);
     }
 
     /**
