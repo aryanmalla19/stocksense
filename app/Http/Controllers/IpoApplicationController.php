275d3979a5c54d4a830a93fdae09745963168d31
Sandip Magar <reone054@gmail.com>
2025-04-07 16:07:09 +0545

    date passed

--- app/Http/Controllers/IpoApplicationController.php
+++ app/Http/Controllers/IpoApplicationController.php
@@ -2,17 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\IPOAppllication\StoreIpoApplicationRequest;
+use App\Http\Requests\IPOApplication\StoreIpoApplicationRequest;
 use App\Http\Resources\IpoApplicationResource;
 
 use App\Models\IpoApplication;
+use Carbon\Carbon;
 
 class IpoApplicationController extends Controller
 {
     public function store(StoreIpoApplicationRequest $request)
     {
-        $ipoApplication = IpoApplication::create($request->validated());
-    
+        $data = $request->validated();
+
+        $data['applied_date'] = Carbon::now();
+        $ipoApplication = IpoApplication::create($data);
+        
         return response()->json([
             'message' => 'Successfully ipo applied',
             'data' => new IpoApplicationResource($ipoApplication),
