39f3320b67be9ad3cf521087333714ab4461b214
Sandip Magar <reone054@gmail.com>
2025-04-07 12:16:46 +0545

    Watchlist resource created

---
+++ app/Http/Resources/WatchListResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class WatchListResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'stock_id' => $this->stock_id,
+            'user_id' => $this->user_id,
+            'symbol' => $this->whenLoaded('stock')->symbol,
+            'company_name' => $this->whenLoaded('stock')->company_name
+        ];
+    }
+}
