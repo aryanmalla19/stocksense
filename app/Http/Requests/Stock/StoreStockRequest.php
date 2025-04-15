4d75d19a6b629ff32da18ce6017485044847a32b
Aryan Malla <aryanmalla19@gmail.com>
2025-04-15 13:24:19 +0545

    validation edited

--- app/Http/Requests/Stock/StoreStockRequest.php
+++ app/Http/Requests/Stock/StoreStockRequest.php
@@ -15,7 +15,7 @@ class StoreStockRequest extends FormRequest
     {
         return [
             'symbol' => 'required|string|max:6|unique:stocks,symbol|regex:/^[A-Za-z]+$/',
-            'company_name' => 'required|string|unique:stocks,company_name',
+            'company_name' => 'required|string|unique:stocks,company_name|regex:/^[A-Za-z\s\.]+$/',
             'sector_id' => 'required|integer|exists:sectors,id',
         ];
     }
