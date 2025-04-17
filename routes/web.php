05215681e7c2a93da5042bdfa943eb6daafdf68a
KEWAL321 <kwlshrestha.com>
2025-04-17 22:56:05 +0545

    added socialite route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\SocialiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return 'welcome';
-});
+});   
+
+Route::controller(SocialiteController::class)->group(
+    function(){
+        Route::get('auth/google', 'googleLogin')->name('auth.google');
+        Route::get('/','googleAuthentication')->name('auth.google-callback');  
+    }
+);
+
+      
