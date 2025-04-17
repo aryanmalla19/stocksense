8b0b15ea7ac98da6ae368230660c48588a7a4aa9
Aryan Malla <aryanmalla19@gmail.com>
2025-04-17 10:50:18 +0545

    enabled columns filtering

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -76,4 +76,24 @@ class Stock extends Model
     {
         return $query->where('symbol','like', "%$symbol%");
     }
+
+    public function scopeSortColumn($query, $column, $direction)
+    {
+        $priceColumns = ['open_price', 'close_price', 'high_price', 'low_price', 'current_price'];
+
+        if (in_array($column, $priceColumns)) {
+            return $query
+                ->join('stock_prices as sp', function ($join) {
+                    $join->on('stocks.id', '=', 'sp.stock_id')
+                        ->whereRaw('sp.id = (
+                         SELECT MAX(id) FROM stock_prices
+                         WHERE stock_prices.stock_id = stocks.id
+                     )');
+                })
+                ->orderBy("sp.$column", $direction)
+                ->select('stocks.*');
+        }
+
+        return $query->orderBy($column, $direction);
+    }
 }
