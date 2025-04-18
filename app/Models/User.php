45c539d1d8124bb037df2e88355d36117177d845
kewal shrestha <kwlshrestha@gmail.com>
2025-04-18 08:56:34 +0545

    added google_id in the fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,6 +28,7 @@ class User extends Authenticatable implements JWTSubject, MustVerifyEmail
         'two_factor_recovery_codes',
         'is_active',
         'role',
+        'google_id'
     ];
 
     protected $hidden = [
