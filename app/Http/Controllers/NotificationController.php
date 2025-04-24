bfcd2d4b351716262e2bf757ad6d8a142a3b567c
Sandip Magar <reone054@gmail.com>
2025-04-24 07:24:00 +0000

    controller to update notification if it belong to user and show

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\NotificationResource;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Notifications\DatabaseNotification;
 
 class NotificationController extends Controller
 {
@@ -29,7 +31,7 @@ class NotificationController extends Controller
             'data' => NotificationResource::collection($combined),
         ], 200);
     }
-    
+
     public function markAllAsRead(): JsonResponse
     {
         $user = auth('api')->user();
@@ -45,5 +47,53 @@ class NotificationController extends Controller
         return response()->json([
             'message' => 'Successfully marked all user\'s notifications as read',
         ], 200);
+    }
+
+    public function update(string $id)
+    {
+        $user = auth('api')->user();
+
+        $notification = $user->notifications()->where('id', $id)->first();
+
+        if (!$notification) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Notification does not belong to the user.',
+            ], 404);
+        }
+
+        if (!is_null($notification->read_at)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Already marked as read',
+            ]);
+        }
+        $notification->markAsRead();
+        return response()->json([
+            'success' => true,
+            'message' => 'Successfully marked notification as read.',
+            'data' => new NotificationResource($notification),
+        ]);
+    }
+
+
+    public function show(string $id)
+    {
+        $user = auth('api')->user();
+
+        $notification = $user->notifications()->where('id', $id)->first();
+
+        if (!$notification) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Notification does not belong to the user.',
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Successfully fetched notification data',
+            'data' => new NotificationResource($notification),
+        ]);
     }
 }
