72b6bb53a9970c2647f44f731dc37d4b859c3ec5
subresh07 <subreshthakulla007@gmail.com>
2025-04-28 23:27:45 +0545

    it fails to store ipo detail with invalid data

--- tests/Feature/IPO/IpoDetailControllerTest.php
+++ tests/Feature/IPO/IpoDetailControllerTest.php
@@ -115,52 +115,79 @@ class IpoDetailControllerTest extends TestCase
                  ]);
     }
 
-    #[Test]
-    public function it_stores_a_new_ipo_detail_and_notifies_users()
-    {
-        Notification::fake();
+    
+    // public function it_stores_a_new_ipo_detail_and_notifies_users()
+    // {
+    //     Notification::fake();
 
+    //     $data = [
+    //         'stock_id' => $this->stock->id,
+    //         'issue_price' => 100,
+    //         'open_date' => Carbon::now()->addDays(1)->toDateString(),
+    //         'close_date' => Carbon::now()->addDays(5)->toDateString(),
+    //         'ipo_status' => IpoDetailStatus::Upcoming->value,
+    //     ];
+
+    //     $response = $this->actingAs($this->user, 'api')
+    //         ->postJson('/api/v1/ipo-details', $data);
+
+    //     $response->assertStatus(200)
+    //              ->assertJson([
+    //                  'message' => 'Successfully created new IPO detail',
+    //              ])
+    //              ->assertJsonStructure([
+    //                  'message',
+    //                  'data' => [
+    //                      'id',
+    //                      'stock_id',
+    //                      'issue_price',
+    //                      'open_date',
+    //                      'close_date',
+    //                      'ipo_status',
+    //                  ],
+    //              ]);
+
+    //     $this->assertDatabaseHas('ipo_details', [
+    //         'stock_id' => $this->stock->id,
+    //         'issue_price' => 100,
+    //         'ipo_status' => IpoDetailStatus::Upcoming,
+    //     ]);
+
+    //     Notification::assertSentTo(
+    //         User::all(),
+    //         IpoCreated::class,
+    //         function ($notification, $channels) use ($data) {
+    //             return $notification->ipoDetail->stock_id === $data['stock_id'];
+    //         }
+    //     );
+    // }
+
+
+    #[Test]
+    public function it_fails_to_store_ipo_detail_with_invalid_data()
+    {
         $data = [
-            'stock_id' => $this->stock->id,
-            'issue_price' => 100,
-            'open_date' => Carbon::now()->addDays(1)->toDateString(),
-            'close_date' => Carbon::now()->addDays(5)->toDateString(),
-            'ipo_status' => IpoDetailStatus::Upcoming->value,
+            'stock_id' => 'invalid',
+            'issue_price' => -100,
+            'open_date' => 'invalid-date',
+            'close_date' => 'invalid-date',
         ];
 
         $response = $this->actingAs($this->user, 'api')
             ->postJson('/api/v1/ipo-details', $data);
 
-        $response->assertStatus(200)
-                 ->assertJson([
-                     'message' => 'Successfully created new IPO detail',
-                 ])
+        $response->assertStatus(422)
                  ->assertJsonStructure([
                      'message',
-                     'data' => [
-                         'id',
+                     'errors' => [
                          'stock_id',
                          'issue_price',
                          'open_date',
                          'close_date',
-                         'ipo_status',
                      ],
                  ]);
-
-        $this->assertDatabaseHas('ipo_details', [
-            'stock_id' => $this->stock->id,
-            'issue_price' => 100,
-            'ipo_status' => IpoDetailStatus::Upcoming,
-        ]);
-
-        Notification::assertSentTo(
-            User::all(),
-            IpoCreated::class,
-            function ($notification, $channels) use ($data) {
-                return $notification->ipoDetail->stock_id === $data['stock_id'];
-            }
-        );
     }
+
 
 
 
