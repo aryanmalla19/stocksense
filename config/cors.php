9d58cd07004964bd69c9668d5d78be1e39a385e4
Aryan Malla <aryanmalla19@gmail.com>
2025-11-08 00:14:43 +0545

    added new url in cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sse-notifications'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'https://stocksense.me', 'http://stocksense.test'],
+    'allowed_origins' => ['http://localhost:3000', 'https://stocksense.me', 'https://www.stocksense.me', 'http://stocksense.test'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => ['Content-Type', 'Authorization'],
