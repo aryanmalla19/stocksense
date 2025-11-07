f48584c6d143c570391973c64cae9c9a7f0be33e
Aryan Malla <aryanmalla19@gmail.com>
2025-11-07 22:12:16 +0545

    updated cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sse-notifications'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'https://stocksense.me'],
+    'allowed_origins' => ['http://localhost:3000', 'https://stocksense.me', 'http://stocksense.test'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => ['Content-Type', 'Authorization'],
